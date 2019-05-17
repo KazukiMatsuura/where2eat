9c923be9ba42d6a8a6036c55a89db400abb36922
Kazuki Matsuura <jtr_basket92@icloud.com>
2019-05-17 16:16:39 +0900

    form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'eateries/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get'lunch/go'
 
   root 'lunch#go'
+
+  post 'eateries/create'
 end
